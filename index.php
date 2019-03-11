83f19a56e5b221ed0128f3d6be2c2baa0948a869
Arjan de Ruijter <rra.am1x.ionic.blok4@gmail.com>
2019-03-11 12:57:11 +0100

    index

--- index.php
+++ index.php
@@ -1,12 +1,18 @@
 <?php
-
-  $servername = "localhost";
-
   define("SERVERNAME", "localhost");
+  define("USERNAME", "root");
+  define("PASSWORD", "");
+  define("DBNAME", "am1c-api-app-2018");
 
-  echo $servername;
+  $conn = mysqli_connect(SERVERNAME, USERNAME, PASSWORD, DBNAME);
 
-  echo SERVERNAME;
+  $sql = "SELECT * FROM `duckinfo`";
 
+  $result =  mysqli_query($conn, $sql);
 
+  $records = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+  // var_dump($records);
+
+  echo json_encode($records);
 ?>
